42b8e72a23cd45c7ff0057f05fc86993753cd0cd
Kanna RS <rskanna19@gmail.com>
2022-03-18 11:46:43 -0700

    create a new challenge ui created , theme changed for login page

--- pages/login.js
+++ pages/login.js
@@ -18,71 +18,73 @@ const Login = () => {
 
     return (
         <div className={s.cls_LoginWrapper}>
-            <div className={s.cls_LoginCont}>
-                <div className = "cls_TitleText">
-                    Login to your Account
-                </div>
-                <div className={s.cls_LoginBody}>
-                <Formik
-                    enableReinitialize
-                    initialValues={{
-                        empId: ""
-                    }}
-                    validationSchema={Yup.object().shape({
-                        empId: Yup.string()
-                            .required("ID is required"),
-                    })}
-                    onSubmit={async (values) => {
-                        handleLogin(values);
-                    }}>
+            <div className={s.cls_LoginWrap2}>
+                <div className={s.cls_LoginCont}>
+                    <div className = "cls_TitleText">
+                        Login to your Account
+                    </div>
+                    <div className={s.cls_LoginBody}>
+                    <Formik
+                        enableReinitialize
+                        initialValues={{
+                            empId: ""
+                        }}
+                        validationSchema={Yup.object().shape({
+                            empId: Yup.string()
+                                .required("ID is required"),
+                        })}
+                        onSubmit={async (values) => {
+                            handleLogin(values);
+                        }}>
 
-                    {({
-                        errors,
-                        handleBlur,
-                        handleChange,
-                        handleSubmit,
-                        isSubmitting,
-                        touched,
-                        values,
-                    }) => (
-                        <form onSubmit={handleSubmit}>
-                            <div>
-                                <TextField
-                                    error={Boolean(touched.email && errors.email)}
-                                    helperText={touched.email && errors.email}
-                                    fullWidth
-                                    required
-                                    className = "cls_LoginInputs"
-                                    label="Employee ID"
-                                    margin="normal"
-                                    name="empId"
-                                    onBlur={handleBlur}
-                                    onChange={handleChange}
-                                    type="text"
-                                    value={values.empId}
-                                    variant = "standard"
-                                    inputProps={{
-                                        className: "cls_LoginInputBox"
-                                    }}
-                                />
-                            </div>
-                            <Box my={2} className = "cls_BtnWrap">
-                                <Button
-                                    className = "cls_Btn"
-                                    color="primary"
-                                    disabled={isSubmitting}
-                                    fullWidth
-                                    size="large"
-                                    type="submit"
-                                    variant="contained"
-                                >
-                                    Sign In
-                                </Button>
-                            </Box>
-                            
-                        </form>
-                    )}
-                </Formik>
+                        {({
+                            errors,
+                            handleBlur,
+                            handleChange,
+                            handleSubmit,
+                            isSubmitting,
+                            touched,
+                            values,
+                        }) => (
+                            <form onSubmit={handleSubmit}>
+                                <div>
+                                    <TextField
+                                        error={Boolean(touched.email && errors.email)}
+                                        helperText={touched.email && errors.email}
+                                        fullWidth
+                                        required
+                                        className = "cls_LoginInputs"
+                                        label="Employee ID"
+                                        margin="normal"
+                                        name="empId"
+                                        onBlur={handleBlur}
+                                        onChange={handleChange}
+                                        type="text"
+                                        value={values.empId}
+                                        variant = "standard"
+                                        inputProps={{
+                                            className: "cls_LoginInputBox"
+                                        }}
+                                    />
+                                </div>
+                                <Box my={2} className = "cls_BtnWrap">
+                                    <Button
+                                        className = "cls_Btn"
+                                        color="primary"
+                                        disabled={isSubmitting}
+                                        fullWidth
+                                        size="large"
+                                        type="submit"
+                                        variant="contained"
+                                    >
+                                        Sign In
+                                    </Button>
+                                </Box>
+                                
+                            </form>
+                        )}
+                    </Formik>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ pages/newChallenge.js
@@ -0,0 +1,129 @@
+import React from "react";
+import s from '../styles/newChallenge.module.css'
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { Box, Button, FormControl, FormHelperText, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+import { useRouter } from "next/router";
+
+const newChallenge = () => {
+
+    const router = useRouter();
+
+    const validate = ( values ) => {
+        let errors = {};
+        if (values.title === "") {
+            Object.assign(errors, {["tile"]: "Title cannot be empty"});
+        }
+        if (values.description === "") {
+            Object.assign(errors, {["description"]: "Description cannot be empty"});
+        }
+        if (values.category === "") {
+            Object.assign(errors, {["category"]: "Category cannot be empty"});
+        }
+        return errors;
+    };
+
+
+    return (
+        <div className={s.cls_LoginWrapper}>
+            <div className={s.cls_LoginWrap2}>
+                <div className={s.cls_LoginCont}>
+                    <div className = "cls_TitleText">
+                        Create A New Challenge
+                    </div>
+                    <div className={s.cls_LoginBody}>
+                    <Formik
+                        enableReinitialize
+                        initialValues={{
+                            title: "",
+                            description: "", 
+                            category: ""
+                        }}
+                        validate={(initialValues) => validate(initialValues)} 
+                        onSubmit={async (values) => {
+                        }}>
+
+                        {({
+                            errors,
+                            handleBlur,
+                            handleChange,
+                            handleSubmit,
+                            isSubmitting,
+                            touched,
+                            values,
+                        }) => (
+                            <form onSubmit={handleSubmit}>
+                                <TextField
+                                    error={Boolean(touched.title && errors.title)}
+                                    helperText={touched.title && errors.title}
+                                    fullWidth
+                                    required
+                                    className = {s.cls_textfield}
+                                    label="Challenge Title"
+                                    margin="normal"
+                                    name="title"
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                    type="text"
+                                    value={values.title}
+                                />
+                                <TextField
+                                    id="outlined-multiline-flexible"
+                                    label="Description"
+                                    required
+                                    className = {s.cls_textfield}
+                                    multiline
+                                    error={Boolean(touched.description && errors.description)}
+                                    helperText={touched.description && errors.description}
+                                    fullWidth
+                                    maxRows={4}
+                                    name="description"
+                                    type="text"
+                                    value={values.description}
+                                    onBlur={handleBlur}
+                                    onChange={handleChange}
+                                />
+                                <TextField
+                                    value={values.category}
+                                    fullWidth
+                                    onBlur={handleBlur}
+                                    required
+                                    className = {s.cls_textfield}
+                                    helperText={touched.category && errors.category}
+                                    error={Boolean(touched.category && errors.category)}
+                                    onChange={(opt, e) => {
+                                            handleChange("category")(opt.target.value);
+                                    }}
+                                    select // tell TextField to render select
+                                    label="Category"
+                                >
+                                    <MenuItem value="health">Health Care</MenuItem>
+                                    <MenuItem value="edu">Education</MenuItem>
+                                    <MenuItem value="comm">Community</MenuItem>
+                                    <MenuItem value="sus">Sustainability</MenuItem>
+                                </TextField>
+                                <Box my={2} className = "cls_BtnWrap">
+                                    <Button
+                                        className = "cls_Btn"
+                                        color="primary"
+                                        disabled={isSubmitting}
+                                        fullWidth
+                                        size="large"
+                                        type="submit"
+                                        variant="contained"
+                                    >
+                                        Create Challenge
+                                    </Button>
+                                </Box>
+                                
+                            </form>
+                        )}
+                    </Formik>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default newChallenge;
